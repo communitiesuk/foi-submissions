2677aa76cae74a4827083414f830f3507e9c187e
Graeme Porteous <graeme@rgbp.co.uk>
2018-03-26 19:28:32 +0100

    Add Rails ENV file
    Taken from Alaveteli this allows ENV variables to be set on app server.
    
    If the RAILS_ENV variable is set then the app will always be forced into
    the correct mode. Even for `rails` or `rake` commands run on the app
    server.

--- config/boot.rb
+++ config/boot.rb
@@ -9,3 +9,10 @@ begin
 rescue LoadError
   STDERR.puts "Couldn't find Bootsnap in the current environment"
 end
+
+# TODO: Remove this. This is a hacky system for having a default environment.
+# It looks for a config/rails_env.rb file, and reads stuff from there if
+# it exists. Put just a line like this in there:
+#   ENV['RAILS_ENV'] = 'production'
+rails_env_file = File.expand_path(File.join(__dir__, 'rails_env.rb'))
+require rails_env_file if File.exist?(rails_env_file)
