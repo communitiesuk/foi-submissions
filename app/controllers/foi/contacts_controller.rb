6ca104bb6d9d46d6bdc051db5424f478a9a77266
Graeme Porteous <graeme@rgbp.co.uk>
2018-04-10 14:23:41 +0100

    Create contact for request
    Link HTML form to the Rails action to create contacts for the current
    FOI request.

--- app/controllers/foi/contacts_controller.rb
+++ app/controllers/foi/contacts_controller.rb
@@ -6,12 +6,36 @@ module Foi
   # a FOI requester.
   #
   class ContactsController < ApplicationController
+    include FindableFoiRequest
+
+    before_action :redirect_to_contact, :new_contact, only: %i[new create]
     def new; end
 
-    def create; end
+    def create
+      if @contact.update(contact_params)
+        redirect_to foi_request_preview_path(@foi_request)
+      else
+        render :new
+      end
+    end
 
     def edit; end
 
     def update; end
+
+    private
+
+    def redirect_to_contact
+      return unless @foi_request.contact
+      redirect_to edit_foi_request_contact_path(@foi_request)
+    end
+
+    def new_contact
+      @contact = @foi_request.build_contact
+    end
+
+    def contact_params
+      params.require(:contact).permit(:full_name, :email)
+    end
   end
 end
--- spec/controllers/foi/contacts_controller_spec.rb
+++ spec/controllers/foi/contacts_controller_spec.rb
@@ -3,19 +3,82 @@
 require 'rails_helper'
 
 RSpec.describe Foi::ContactsController, type: :controller do
+  let(:foi_request) { build_stubbed(:foi_request) }
+  let(:contact) { build_stubbed(:contact) }
+  let(:valid_params) { { full_name: 'Spock', email: 'spock@localhost' } }
+  let(:invalid_params) { { invalid: true } }
+
+  before do
+    allow(FoiRequest).to receive(:find).with('1').and_return(foi_request)
+  end
+
   describe 'GET #new' do
     subject { get :new, params: { request_id: '1' } }
+    before { allow(foi_request).to receive(:contact).and_return(nil) }
 
-    it 'returns http success' do
-      is_expected.to have_http_status(200)
+    context 'existing contact' do
+      before { allow(foi_request).to receive(:contact).and_return(double) }
+
+      it 'redirects to edit contact' do
+        is_expected.to redirect_to(edit_foi_request_contact_path(foi_request))
+      end
+    end
+
+    context 'new contact' do
+      it 'returns http success' do
+        is_expected.to have_http_status(200)
+      end
     end
   end
 
   describe 'POST #create' do
-    subject { post :create, params: { request_id: '1' } }
+    before do
+      allow(foi_request).to receive(:contact).and_return(nil)
+      allow(foi_request).to receive(:build_contact).and_return(contact)
+    end
 
-    it 'returns http success' do
-      is_expected.to have_http_status(204)
+    context 'existing contact' do
+      subject do
+        post :create, params: { request_id: '1' }
+      end
+      before { allow(foi_request).to receive(:contact).and_return(double) }
+
+      it 'does not receive attributes' do
+        expect(contact).to_not receive(:update)
+        subject
+      end
+
+      it 'redirects to edit contact' do
+        is_expected.to redirect_to(edit_foi_request_contact_path(foi_request))
+      end
+    end
+
+    context 'valid parameters' do
+      subject do
+        post :create, params: { request_id: '1', contact: valid_params }
+      end
+      before { allow(contact).to receive(:update).and_return(true) }
+
+      it 'receives valid attributes' do
+        expect(contact).to receive(:update).
+          with(ActionController::Parameters.new(valid_params).permit!)
+        subject
+      end
+
+      it 'redirects to foi_request' do
+        is_expected.to redirect_to(foi_request_preview_path(foi_request))
+      end
+    end
+
+    context 'invalid parameters' do
+      subject do
+        post :create, params: { request_id: '1', contact: invalid_params }
+      end
+      before { allow(contact).to receive(:update).and_return(false) }
+
+      it 'returns http success' do
+        is_expected.to have_http_status(200)
+      end
     end
   end
 
