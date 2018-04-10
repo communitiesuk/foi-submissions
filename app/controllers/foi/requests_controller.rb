0999812d807c0c212a93f70c50b934d7c34d066a
Graeme Porteous <graeme@rgbp.co.uk>
2018-04-10 14:22:57 +0100

    Create requests action
    Link HTML form to the Rails action to create FOI requests.

--- app/controllers/foi/requests_controller.rb
+++ app/controllers/foi/requests_controller.rb
@@ -5,14 +5,32 @@ module Foi
   # This controller is responsible for creating and updating of FOI requests.
   #
   class RequestsController < ApplicationController
+    before_action :new_foi_request, only: %i[new create]
+
     def index; end
 
     def new; end
 
-    def create; end
+    def create
+      if @foi_request.update(foi_request_params)
+        redirect_to foi_request_suggestions_path(@foi_request)
+      else
+        render :new
+      end
+    end
 
     def edit; end
 
     def update; end
+
+    private
+
+    def new_foi_request
+      @foi_request = FoiRequest.new
+    end
+
+    def foi_request_params
+      params.require(:foi_request).permit(:body)
+    end
   end
 end
--- spec/controllers/foi/requests_controller_spec.rb
+++ spec/controllers/foi/requests_controller_spec.rb
@@ -3,6 +3,10 @@
 require 'rails_helper'
 
 RSpec.describe Foi::RequestsController, type: :controller do
+  let(:foi_request) { build_stubbed(:foi_request) }
+  let(:valid_params) { { body: 'A request body' } }
+  let(:invalid_params) { { invalid: true } }
+
   describe 'GET #index' do
     subject { get :index }
 
@@ -13,6 +17,7 @@ RSpec.describe Foi::RequestsController, type: :controller do
 
   describe 'GET #new' do
     subject { get :new }
+    before { allow(FoiRequest).to receive(:new).and_return(foi_request) }
 
     it 'returns http success' do
       is_expected.to have_http_status(200)
@@ -20,10 +25,36 @@ RSpec.describe Foi::RequestsController, type: :controller do
   end
 
   describe 'POST #create' do
-    subject { post :create }
+    context 'valid parameters' do
+      subject { post :create, params: { foi_request: valid_params } }
 
-    it 'returns http success' do
-      is_expected.to have_http_status(204)
+      before do
+        allow(FoiRequest).to receive(:new).and_return(foi_request)
+        allow(foi_request).to receive(:update).and_return(true)
+      end
+
+      it 'receives valid attributes' do
+        expect(foi_request).to receive(:update).
+          with(ActionController::Parameters.new(valid_params).permit!)
+        subject
+      end
+
+      it 'redirects to suggestions' do
+        is_expected.to redirect_to(foi_request_suggestions_path(foi_request))
+      end
+    end
+
+    context 'invalid parameters' do
+      subject { post :create, params: { foi_request: invalid_params } }
+
+      before do
+        allow(FoiRequest).to receive(:new).and_return(foi_request)
+        allow(foi_request).to receive(:update).and_return(false)
+      end
+
+      it 'returns http success' do
+        is_expected.to have_http_status(200)
+      end
     end
   end
 
