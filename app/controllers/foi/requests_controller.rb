b3c81382cda81d170f534b781edf143356250197
Graeme Porteous <graeme@rgbp.co.uk>
2018-04-10 14:24:12 +0100

    Edit requests
    Allow user to edit an existing FOI request.

--- app/controllers/foi/requests_controller.rb
+++ app/controllers/foi/requests_controller.rb
@@ -6,6 +6,7 @@ module Foi
   #
   class RequestsController < ApplicationController
     before_action :new_foi_request, only: %i[new create]
+    before_action :find_foi_request, only: %i[edit update]
 
     def index; end
 
@@ -21,12 +22,22 @@ module Foi
 
     def edit; end
 
-    def update; end
+    def update
+      if @foi_request.update(foi_request_params)
+        redirect_to foi_request_suggestions_path(@foi_request)
+      else
+        render :edit
+      end
+    end
 
     private
 
     def new_foi_request
       @foi_request = FoiRequest.new
+    end
+
+    def find_foi_request
+      @foi_request = FoiRequest.find(params.require(:id))
     end
 
     def foi_request_params
--- spec/controllers/foi/requests_controller_spec.rb
+++ spec/controllers/foi/requests_controller_spec.rb
@@ -61,16 +61,42 @@ RSpec.describe Foi::RequestsController, type: :controller do
   describe 'GET #edit' do
     subject { get :edit, params: { id: '1' } }
 
+    before do
+      allow(FoiRequest).to receive(:find).with('1').and_return(foi_request)
+    end
+
     it 'returns http success' do
-      is_expected.to have_http_status(200)
+      expect(response).to have_http_status(200)
     end
   end
 
   describe 'PUT #update' do
-    subject { put :update, params: { id: '1' } }
+    before do
+      allow(FoiRequest).to receive(:find).with('1').and_return(foi_request)
+    end
 
-    it 'returns http success' do
-      is_expected.to have_http_status(204)
+    context 'valid parameters' do
+      subject { put :update, params: { id: '1', foi_request: valid_params } }
+      before { allow(foi_request).to receive(:update).and_return(true) }
+
+      it 'receives valid attributes' do
+        expect(foi_request).to receive(:update).
+          with(ActionController::Parameters.new(valid_params).permit!)
+        subject
+      end
+
+      it 'redirects to suggestions' do
+        is_expected.to redirect_to(foi_request_suggestions_path(foi_request))
+      end
+    end
+
+    context 'invalid parameters' do
+      subject { put :update, params: { id: '1', foi_request: invalid_params } }
+      before { allow(foi_request).to receive(:update).and_return(false) }
+
+      it 'returns http success' do
+        is_expected.to have_http_status(200)
+      end
     end
   end
 end
