2a489668ad17396833efe09cd701011322edf4c1
Graeme Porteous <graeme@rgbp.co.uk>
2018-04-10 14:50:00 +0100

    No documentation for some classes
    Skipped for default Rails classes

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ require 'sprockets/railtie'
 Bundler.require(*Rails.groups)
 
 module FoiForCouncils
-  class Application < Rails::Application
+  class Application < Rails::Application #:nodoc:
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
