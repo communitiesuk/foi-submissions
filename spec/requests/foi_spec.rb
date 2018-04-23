d850a02042a63f693325fdc6fcb5257dd3d4d934
Graeme Porteous <graeme@rgbp.co.uk>
2018-04-23 14:13:13 +0100

    Remove unneeded spec setup

--- spec/requests/foi_spec.rb
+++ spec/requests/foi_spec.rb
@@ -18,15 +18,6 @@ RSpec.describe 'foi namespace', type: :request do
   end
 
   describe 'GET /foi/request/:id/contact' do
-    include_context 'FOI Request Scope'
-
-    let(:foi_request) { build_stubbed(:foi_request, contact: nil) }
-
-    before do
-      allow(foi_request_scope).to receive(:find_by).
-        with(id: '1').and_return(foi_request)
-    end
-
     it 'redirects to requests index' do
       get '/foi/requests/1/contact'
       expect(response).to redirect_to('/foi/requests/1/contact/new')
