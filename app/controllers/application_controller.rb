3cacb6e53830d4653f49af37d87a697079edff38
Graeme Porteous <graeme@rgbp.co.uk>
2018-04-27 11:09:30 +0100

    Revert "Add HTTP auth for whole application"
    This reverts commit c6cda1f788925ba6ba59e4eb66e6b868915e1c42.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,4 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base # :nodoc:
-  if ENV.key?('HTTP_AUTH_PASSWORD')
-    http_basic_authenticate_with name: 'hackney-foi',
-                                 password: ENV['HTTP_AUTH_PASSWORD']
-  end
 end
