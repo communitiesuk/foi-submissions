7ba12287ad5d51cd62ddc2c70cf671d85a4c153f
Graeme Porteous <graeme@rgbp.co.uk>
2018-04-27 15:10:23 +0100

    Change root URLs
    In preperation of changing to a singlar resource for the Requests which
    would result in the loss of the index route. Instead we are replacing it
    here with the `/foi` route.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root to: redirect('foi/requests')
+  root to: redirect('foi')
 
   namespace :foi do
-    root to: redirect('foi/requests')
+    root to: 'requests#index'
     resources :requests, except: %i[show destroy] do
+      root to: redirect('foi/request/new')
       resources :suggestions, only: %i[index]
       get 'contact', to: redirect('foi/requests/%{request_id}/contact/new')
       resource :contact, except: %i[show destroy]
--- spec/requests/foi_spec.rb
+++ spec/requests/foi_spec.rb
@@ -4,21 +4,21 @@ require 'rails_helper'
 
 RSpec.describe 'foi namespace', type: :request do
   describe 'GET /' do
-    it 'redirects to requests index' do
+    it 'redirects to foi namespace' do
       get '/'
-      expect(response).to redirect_to('/foi/requests')
+      expect(response).to redirect_to('/foi')
     end
   end
 
-  describe 'GET /foi' do
-    it 'redirects to requests index' do
-      get '/foi'
-      expect(response).to redirect_to('/foi/requests')
+  describe 'GET /foi/requests' do
+    it 'redirects to new request' do
+      get '/foi/requests'
+      expect(response).to redirect_to('/foi/request/new')
     end
   end
 
-  describe 'GET /foi/request/:id/contact' do
-    it 'redirects to requests index' do
+  describe 'GET /foi/requests/:id/contact' do
+    it 'redirects to new request contact' do
       get '/foi/requests/1/contact'
       expect(response).to redirect_to('/foi/requests/1/contact/new')
     end
