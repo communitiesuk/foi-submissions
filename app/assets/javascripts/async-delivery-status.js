8252e8d445e67b9ef899dcd4d23597aa12523812
Zarino Zappia <mail@zarino.co.uk>
2018-05-03 17:48:06 +0100

    Prototype live-reloading delivery status UI

---
+++ app/assets/javascripts/async-delivery-status.js
@@ -0,0 +1,12 @@
+$('.js-async-delivery-status').each(function(){
+    var $box = $(this);
+
+    // The "generic" and "delivered" states will already have been hidden by
+    // the CSS, and the "loading" state shown. So we just need wait a random
+    // amount of time, then hide "loading" and show "delivered";
+    var randomDelay = Math.floor( Math.random() * 5000 );
+    setTimeout(function(){
+        $box.find('.async-delivery-status--loading').hide();
+        $box.find('.async-delivery-status--delivered').show();
+    }, randomDelay);
+});
