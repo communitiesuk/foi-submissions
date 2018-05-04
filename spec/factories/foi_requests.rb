91d36a0e4b2444d12d7a2f8406681c914a2dc0ac
Graeme Porteous <graeme@rgbp.co.uk>
2018-05-04 11:38:37 +0100

    Don't save/create associated factories
    It was unexpected that associated objects were saved and and test suite
    passed when they are just built.

--- spec/factories/foi_requests.rb
+++ spec/factories/foi_requests.rb
@@ -3,18 +3,18 @@
 FactoryBot.define do
   factory :foi_request do
     body 'How much did you spend on cycling infrastructure last year?'
-    contact
+    association :contact, strategy: :build
 
     trait :unqueued do
-      association :submission, :unqueued
+      association :submission, :unqueued, strategy: :build
     end
 
     trait :queued do
-      association :submission, :queued
+      association :submission, :queued, strategy: :build
     end
 
     trait :delivered do
-      association :submission, :delivered
+      association :submission, :delivered, strategy: :build
     end
   end
 end
