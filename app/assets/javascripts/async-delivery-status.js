880182dc9c0ba93954f0fed1b40faeaebfec56e1
Graeme Porteous <graeme@rgbp.co.uk>
2018-05-09 14:55:03 +0100

    Poll for the submission reference
    We try 5 times to seen the requests has been sent and reference stored
    on the submission record. There is a small increase in wait time between
    attempts as to reduce load on the server, in total we wait ~5 seconds
    before showing the generic message.

--- app/assets/javascripts/async-delivery-status.js
+++ app/assets/javascripts/async-delivery-status.js
@@ -2,13 +2,42 @@
 
 $('.js-async-delivery-status').each(function () {
   var $box = $(this)
+  var source = $box.data('source')
+  var $ref = $('.js-submission-reference', $box)
 
   // The "generic" and "delivered" states will already have been hidden by
-  // the CSS, and the "loading" state shown. So we just need wait a random
-  // amount of time, then hide "loading" and show "delivered";
-  var randomDelay = Math.floor(Math.random() * 5000)
-  setTimeout(function () {
-    $box.find('.async-delivery-status--loading').hide()
-    $box.find('.async-delivery-status--delivered').show()
-  }, randomDelay)
+  // the CSS, and the "loading" state shown
+  var runs = 0
+  var runTimer = function () {
+    runs++
+
+    // Abort after 5 attempts (which takes approx 5 seconds) and just show the
+    // generic message
+    if (runs > 5) {
+      $box.find('.async-delivery-status--loading').hide()
+      $box.find('.async-delivery-status--generic').show()
+      return
+    }
+
+    setTimeout(function () {
+      $.get(source, function (data) {
+        var ref = data.reference
+
+        if (ref) {
+          // Once went have a reference show the delivered message
+          $ref.text(ref)
+
+          $box.find('.async-delivery-status--loading').hide()
+          $box.find('.async-delivery-status--delivered').show()
+        } else {
+          // No reference this this so requeue timer
+          runTimer()
+        }
+      })
+      // Go easy on the server and increase the length of time between each
+      // attempt
+    }, 500 + (250 * runs))
+  }
+
+  runTimer()
 })
