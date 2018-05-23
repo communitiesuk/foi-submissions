5e6394406d1435e6e895740ba42570e4bcbb4f85
Graeme Porteous <graeme@rgbp.co.uk>
2018-05-23 13:51:32 +0100

    Remove custom CuratedLink#keywords
    This was let over from a different all-ruby searching approach but isn't
    needed anymore as keywords will be split within the PG search query.

--- app/models/curated_link.rb
+++ app/models/curated_link.rb
@@ -3,8 +3,4 @@
 # Resources added by staff that appear in the Suggestions to users.
 class CuratedLink < ApplicationRecord
   validates :title, :url, presence: true
-
-  def keywords
-    self[:keywords].to_s.split
-  end
 end
