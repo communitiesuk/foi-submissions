mysociety/foi-for-councils

Record job ID on submissions
This will help with debugging delivery issues.

db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_02_090749) do
+ActiveRecord::Schema.define(version: 2018_05_15_131458) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2018_05_02_090749) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "reference"
+    t.string "jid"
   end
 
   add_foreign_key "foi_requests", "contacts"

communitiesuk/foi-submissions

db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_02_090749) do
+ActiveRecord::Schema.define(version: 2018_05_15_131458) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2018_05_02_090749) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "reference"
+    t.string "job_id"
   end
 
   add_foreign_key "foi_requests", "contacts"