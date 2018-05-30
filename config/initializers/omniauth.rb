eb17f65b1583a4a7d21838e4320f00c9c6ae4179
Graeme Porteous <graeme@rgbp.co.uk>
2018-05-30 13:55:07 +0100

    !squash Added OmniAuth with Google OAuth2 strategy
    Force re-authenication whenever strategy is used.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,6 @@
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'],
-           name: 'google', hd: %w[mysociety.org hackney.gov.uk]
+           name: 'google', hd: %w[mysociety.org hackney.gov.uk],
+           prompt: 'select_account'
 end
