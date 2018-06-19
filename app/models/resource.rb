76ceb29cf67b8fd7fecb54fc5c5ac3601946dcf1
Graeme Porteous <graeme@rgbp.co.uk>
2018-06-19 16:32:05 +0100

    Add CSV related methods to Resource
    Taken from CuratedLink.

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -7,4 +7,17 @@ class Resource < ApplicationRecord
   belongs_to :resource, polymorphic: true
 
   delegate :foi_suggestions, :id, :created_at, :updated_at, to: :resource
+  delegate :shown, :click_rate, :answer_rate, to: :statistics
+
+  def self.csv_columns
+    %i[id title url keywords
+       shown click_rate answer_rate
+       created_at updated_at]
+  end
+
+  private
+
+  def statistics
+    @statistics ||= OpenStruct.new(foi_suggestions.statistics)
+  end
 end
--- spec/models/resource_spec.rb
+++ spec/models/resource_spec.rb
@@ -13,6 +13,12 @@ RSpec.describe Resource, type: :model do
     end
   end
 
+  describe '.csv_columns' do
+    it 'returns an array' do
+      expect(described_class.csv_columns).to be_a Array
+    end
+  end
+
   describe 'resource delegate methods' do
     before do
       resource.resource = CuratedLink.new(
@@ -35,6 +41,30 @@ RSpec.describe Resource, type: :model do
     describe '#updated_at' do
       subject { resource.updated_at }
       it { is_expected.to eq Time.new(2018, 1, 1, 0, 1).utc }
+    end
+  end
+
+  describe 'statistics delegate methods' do
+    before do
+      expect(resource).to(
+        receive_message_chain(:foi_suggestions, :statistics).
+        and_return(shown: 3, click_rate: 0.66, answer_rate: 0.33)
+      )
+    end
+
+    describe '#shown' do
+      subject { resource.shown }
+      it { is_expected.to eq 3 }
+    end
+
+    describe '#click_rate' do
+      subject { resource.click_rate }
+      it { is_expected.to eq 0.66 }
+    end
+
+    describe '#answer_rate' do
+      subject { resource.answer_rate }
+      it { is_expected.to eq 0.33 }
     end
   end
 end
