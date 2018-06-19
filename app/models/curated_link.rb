d3fb320f9c911b904562256f8af00b5332ef1b79
Graeme Porteous <graeme@rgbp.co.uk>
2018-06-19 16:32:05 +0100

    Remove CSV related methods from CuratedLink
    These methods aren't being used anymore.

--- app/models/curated_link.rb
+++ app/models/curated_link.rb
@@ -10,16 +10,6 @@ class CuratedLink < ApplicationRecord
 
   scope :active, -> { where(destroyed_at: nil) }
 
-  delegate :shown, :click_rate, :answer_rate, to: :statistics
-
-  def statistics
-    @statistics ||= OpenStruct.new(foi_suggestions.statistics)
-  end
-
-  def self.csv_columns
-    %i[id title url keywords shown click_rate answer_rate created_at updated_at]
-  end
-
   def soft_destroy
     update(destroyed_at: Time.zone.now)
   end
--- spec/models/curated_link_spec.rb
+++ spec/models/curated_link_spec.rb
@@ -31,36 +31,6 @@ RSpec.describe CuratedLink, type: :model do
     end
   end
 
-  describe '.csv_columns' do
-    it 'returns an array' do
-      expect(described_class.csv_columns).to be_a Array
-    end
-  end
-
-  describe 'statistics delegate methods' do
-    before do
-      expect(curated_link).to(
-        receive_message_chain(:foi_suggestions, :statistics).
-        and_return(shown: 3, click_rate: 0.66, answer_rate: 0.33)
-      )
-    end
-
-    describe '#shown' do
-      subject { curated_link.shown }
-      it { is_expected.to eq 3 }
-    end
-
-    describe '#click_rate' do
-      subject { curated_link.click_rate }
-      it { is_expected.to eq 0.66 }
-    end
-
-    describe '#answer_rate' do
-      subject { curated_link.answer_rate }
-      it { is_expected.to eq 0.33 }
-    end
-  end
-
   describe 'scopes' do
     let!(:active) { create(:curated_link, destroyed_at: nil) }
     let!(:destroyed) { create(:curated_link, destroyed_at: Time.zone.now) }
