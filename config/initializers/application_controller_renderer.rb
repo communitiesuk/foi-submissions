e2854d2f32ee6c00dd38fa45f9ca9ad47fc4db21
Graeme Porteous <graeme@rgbp.co.uk>
2018-06-20 15:46:01 +0100

    Move CSV rendering into service object
    Allow easier testing of this code.

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -9,29 +9,7 @@
 #   )
 # end
 
-require 'csv'
-
-ActionController::Renderers.add :csv do |objects, _options|
-  klass = objects.first&.class
-  unless klass.respond_to?(:csv_columns)
-    raise StandardError, "#{klass} class doesn't respond to `csv_columns`"
-  end
-
-  def convert_to_csv(object)
-    object.class.csv_columns.map do |column|
-      value = object.public_send(column)
-      case value
-      when Time then value.to_s(:db)
-      else value
-      end
-    end
-  end
-
-  csv_string = CSV.generate do |csv|
-    headers = klass.csv_columns
-    csv << headers if headers
-    objects.each { |object| csv << convert_to_csv(object) }
-  end
-
-  send_data csv_string, type: Mime[:csv], filename: 'export.csv'
+ActionController::Renderers.add :csv do |objects, options|
+  export = CSVExporter.new(objects, options)
+  send_data export.data, type: Mime[:csv], filename: 'export.csv'
 end
---
+++ spec/services/csv_exporter_spec.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe CSVExporter, type: :service do
+  Mock = Struct.new(:name, :time)
+  class MockEvent < Mock
+    def self.csv_columns
+      %i[name time]
+    end
+  end
+
+  let(:objects) { [MockEvent.new('event', Time.utc(2018, 6, 20, 15, 30))] }
+  subject(:export) { described_class.new(objects) }
+
+  describe 'initialisation' do
+    it 'assigns objects' do
+      expect(export.objects).to eq objects
+    end
+
+    it 'requires class which response to csv_columns' do
+      expect { export }.to_not raise_error
+      expect { described_class.new(['foo']) }.to raise_error(CSVExporter::Error)
+    end
+  end
+
+  describe '#data' do
+    subject(:data) { export.data }
+    let(:lines) { data.split("\n") }
+
+    it 'generates valid CSV string' do
+      expect { CSV.parse(data).inspect }.to_not raise_error
+    end
+
+    it 'includes CSV headers' do
+      expect(lines.first).to eq 'name,time'
+    end
+
+    it 'maps Times to DB format' do
+      expect(lines.last).to eq 'event,2018-06-20 15:30:00'
+    end
+  end
+end
