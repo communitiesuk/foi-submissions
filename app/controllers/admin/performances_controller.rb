mysociety/foi-for-councils

Add Performance admin controller

app/controllers/admin/performances_controller.rb
@@ -1,0 +1,31 @@
+# frozen_string_literal: true
+
+module Admin
+  ##
+  # This controller is responsible for updating the performance percentage
+  #
+  class PerformancesController < AdminController
+    def index; end
+
+    def new
+      @performance = Performance.new
+    end
+
+    def create
+      @performance = Performance.new
+
+      if @performance.update(performance_params)
+        redirect_to admin_performances_path,
+                    notice: 'Performance percentage successfully updated'
+      else
+        render :new
+      end
+    end
+
+    private
+
+    def performance_params
+      params.require(:performance).permit(:percentage)
+    end
+  end
+end

config/routes.rb
@@ -28,6 +28,7 @@ Rails.application.routes.draw do
         resource :export, only: [:show], format: 'csv'
       end
     end
+    resources :performances, only: %i[index new create]
   end
 
   namespace :health do

spec/controllers/admin/performances_controller_spec.rb
@@ -1,0 +1,71 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Admin::PerformancesController, type: :controller do
+  let(:performance) { build_stubbed(:performance) }
+  let(:valid_params) { { percentage: '72' } }
+  let(:invalid_params) { { invalid: true } }
+
+  let(:session) do
+    { current_user: 123, authenticated_until: (Time.zone.now + 1.hour).to_i }
+  end
+
+  before do
+    allow(User).to receive(:find_by).with(uid: 123).and_return(build(:user))
+  end
+
+  describe 'GET #index' do
+    subject { get :index, session: session }
+
+    it 'returns http success' do
+      is_expected.to have_http_status(200)
+    end
+  end
+
+  describe 'GET #new' do
+    subject { get :new, session: session }
+
+    it 'returns http success' do
+      is_expected.to have_http_status(200)
+    end
+  end
+
+  describe 'POST #create' do
+    before { allow(Performance).to receive(:new).and_return(performance) }
+
+    context 'valid parameters' do
+      subject do
+        post :create, params: { performance: valid_params }, session: session
+      end
+      before { allow(performance).to receive(:update).and_return(true) }
+
+      it 'receives valid attributes' do
+        expect(performance).to receive(:update).
+          with(ActionController::Parameters.new(valid_params).permit!)
+        subject
+      end
+
+      it 'redirects to index' do
+        is_expected.to redirect_to(admin_performances_path)
+      end
+
+      it 'sets flash notice' do
+        subject
+        expect(flash.notice).to_not be_nil
+      end
+    end
+
+    context 'invalid parameters' do
+      subject do
+        post :create, params: { performance: invalid_params },
+                      session: session
+      end
+      before { allow(performance).to receive(:update).and_return(false) }
+
+      it 'returns http success' do
+        is_expected.to have_http_status(200)
+      end
+    end
+  end
+end

communitiesuk/foi-submissions

app/controllers/admin/performances_controller.rb
@@ -1,0 +1,31 @@
+# frozen_string_literal: true
+
+module Admin
+  ##
+  # This controller is responsible for updating the performance percentage
+  #
+  class PerformancesController < AdminController
+    def show; end
+
+    def new
+      @performance = Performance.new
+    end
+
+    def create
+      @performance = Performance.new
+
+      if @performance.update(performance_params)
+        redirect_to admin_performance_path,
+                    notice: 'Performance percentage successfully updated'
+      else
+        render :new
+      end
+    end
+
+    private
+
+    def performance_params
+      params.require(:performance).permit(:percentage)
+    end
+  end
+end

config/routes.rb
@@ -28,6 +28,7 @@ Rails.application.routes.draw do
         resource :export, only: [:show], format: 'csv'
       end
     end
+    resource :performance, only: %i[show new create]
   end
 
   namespace :health do

spec/controllers/admin/performances_controller_spec.rb
@@ -1,0 +1,71 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Admin::PerformancesController, type: :controller do
+  let(:performance) { build_stubbed(:performance) }
+  let(:valid_params) { { percentage: '72' } }
+  let(:invalid_params) { { invalid: true } }
+
+  let(:session) do
+    { current_user: 123, authenticated_until: (Time.zone.now + 1.hour).to_i }
+  end
+
+  before do
+    allow(User).to receive(:find_by).with(uid: 123).and_return(build(:user))
+  end
+
+  describe 'GET #show' do
+    subject { get :show, session: session }
+
+    it 'returns http success' do
+      is_expected.to have_http_status(200)
+    end
+  end
+
+  describe 'GET #new' do
+    subject { get :new, session: session }
+
+    it 'returns http success' do
+      is_expected.to have_http_status(200)
+    end
+  end
+
+  describe 'POST #create' do
+    before { allow(Performance).to receive(:new).and_return(performance) }
+
+    context 'valid parameters' do
+      subject do
+        post :create, params: { performance: valid_params }, session: session
+      end
+      before { allow(performance).to receive(:update).and_return(true) }
+
+      it 'receives valid attributes' do
+        expect(performance).to receive(:update).
+          with(ActionController::Parameters.new(valid_params).permit!)
+        subject
+      end
+
+      it 'redirects to show' do
+        is_expected.to redirect_to(admin_performance_path)
+      end
+
+      it 'sets flash notice' do
+        subject
+        expect(flash.notice).to_not be_nil
+      end
+    end
+
+    context 'invalid parameters' do
+      subject do
+        post :create, params: { performance: invalid_params },
+                      session: session
+      end
+      before { allow(performance).to receive(:update).and_return(false) }
+
+      it 'returns http success' do
+        is_expected.to have_http_status(200)
+      end
+    end
+  end
+end